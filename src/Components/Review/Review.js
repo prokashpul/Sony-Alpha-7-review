b70b74d93f864c172f149351a78eaee0577a506f
prokash <pprokash102@gmail.com>
2022-04-03 23:53:28 +0600

    review page add card and data load

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-const Review = ({ reviews }) => {
-  const { name, review, img, star } = reviews;
+const Review = ({ review }) => {
+  const { name, reviews, img, rating } = review;
   return (
-    <div>
-      <div>
-        <img src={img} alt={name} className="w-20 h-20 rounded-full" />
-        <h2>{name}</h2>
+    <div className="shadow hover:shadow-lg duration-500 p-10  rounded-lg border-indigo-600 border">
+      <div className="flex gap-4 items-center justify-start">
+        <img src={img} alt={name} className="w-14 h-14 rounded-full" />
+        <h2 className="text-2xl font-mono font-semibold">{name}</h2>
       </div>
+      <h4 className={"text-2xl my-2"}>
+        Rating :{" "}
+        <span
+          className={rating >= 5 ? "text-red-700 font-bold" : "text-gray-900"}
+        >
+          {rating}
+
+          <small>star</small>
+        </span>
+      </h4>
+      <p>{reviews}</p>
     </div>
   );
 };
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -3,16 +3,18 @@ import useReviews from "../../Hooks/useReviews";
 import Review from "../Review/Review";
 
 const Reviews = () => {
-  const [reviews, setReviews] = useReviews();
+  const [reviews] = useReviews();
   console.log(reviews);
   return (
-    <section className="md:w-[85%] md:w-[95%] mx-auto my-20">
+    <section className="md:w-[85%] w-[95%] mx-auto my-10 md:my-20">
       <h2 className="text-center font-bold text-3xl md:text-5xl ">
         All Reviews ({reviews.length})
       </h2>
-      {reviews.map((reviews) => (
-        <Review reviews={reviews} key={reviews.id}></Review>
-      ))}
+      <div className="grid md:grid-cols-3 md:mt-20 mt-10 gap-5">
+        {reviews.map((reviews) => (
+          <Review review={reviews} key={reviews.id}></Review>
+        ))}
+      </div>
     </section>
   );
 };
