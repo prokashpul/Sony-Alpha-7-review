a9f65b6d0ec6780336a5a6222af1f5a95338735c
prokash <pprokash102@gmail.com>
2022-04-04 07:25:57 +0600

    blog faq page update

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
-  return <div>Blogs</div>;
+  const [faqs, setFaqs] = useState([]);
+  useEffect(() => {
+    fetch("faqData.json")
+      .then((res) => res.json())
+      .then((data) => setFaqs(data));
+  }, []);
+  return (
+    <div className="mb-20">
+      <h2 className="text-center font-black text-5xl my-10">Blogs</h2>
+      <div className="md:w-[85%] w-[95%] mx-auto my-10">
+        {faqs.map((faq) => (
+          <Blog faq={faq} key={faq.id}></Blog>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
